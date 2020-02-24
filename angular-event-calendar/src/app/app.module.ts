b6902a40dbb1b21645fae2d9d6d7f7b236c31375
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-02-23 22:00:07 -0500

    add app module imports

--- angular-event-calendar/src/app/app.module.ts
+++ angular-event-calendar/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [CommonModule, FormsModule, BrowserModule, HttpClientModule],
+  exports: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
